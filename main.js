731d4e7d83dce9fc149cc32aa4e47419d0c670b9
Eduardo Soares <148549393+EduSoaresMT@users.noreply.github.com>
2024-02-24 06:41:45 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $('ul').css('border-top-right-radius', '50px');
+    $('ul').css('border-top-left-radius', '50px');
+    $('#slide').click(function(){
+      $('form').slideToggle();
+      $('ul').css('border-radius', '0px');
+    })
+  
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+    const adicionarConteudo = $('#compromisso').val(); 
+    const novoItemLista = $('<li></li>').text(adicionarConteudo);
+    $('ul').append(novoItemLista);
+    $('#compromisso').val('');
+  });
+  
+  $('ul').on('click', 'li', function() {
+    $(this).toggleClass('concluido');
+    if ($(this).hasClass('concluido')) {
+        $(this).css('text-decoration', 'line-through');
+        $(this).css('font-weight', 'bold');
+    } else {
+        $(this).css('text-decoration', 'none');
+        $(this).css('font-weight', 'normal');
+    }
+  });
+  })
+  
+  
